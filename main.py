4e91ad71e262be2bfe52de7489fc26f54f37aef0
ThomasG <tom.glass@icloud.com>
2014-05-07 16:47:04 +0000

    Start up Stuff /YOLO/

---
+++ main.py
@@ -0,0 +1,9 @@
+from onlinenet import onlinenet
+
+apikey = '2b5b1f962f2fcc1dc094c0b7c0b444dd7bac8381'
+#apikey = 'f76be1f975fc03e08f1e43ac37dabdbe8d52f435'
+onlineapi = onlinenet()
+
+print onlineapi.ddos(apikey)
+
+#print onlineapi.ddos_info("183414", apikey)
